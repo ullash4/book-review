bc79d5370befd3c605cf61fbc050b4a16bf1e8fe
ullash4 <programmerullash@gmail.com>
2022-04-04 16:42:24 +0600

    home page created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-import Home from './Components/Home/Home'
+import Home from './Components/Home/Home';
+import Review from './Components/Review/Review';
+import Dashboard from './Components/Dasboard/Dashboard';
+import Blog from './Components/Blog/Blog';
+import Contact from './Components/Contact/Contact'
 
 function App() {
   return (
@@ -9,6 +13,11 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/review" element={<Review />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </div>
   );
